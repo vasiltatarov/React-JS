1b8b525c7dee2f5d0bc21929459f1276c8399493
vasiltatarov <vasiltatarov3@gmail.com>
2021-05-16 19:34:49 +0300

    create new project and implement navbar

---
+++ projects/react-website-1/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/Navbar.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
